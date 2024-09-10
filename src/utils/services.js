9f31033663cc276a0694083b3190225a3286de2d
alexandro.berdejo <alexandro.berdejo@gmail.com>
2024-09-10 03:26:53 -0600

    FirstCommmitStructurandCarpetas

---
+++ src/utils/services.js
@@ -0,0 +1,16 @@
+// src/utils/services.js
+ const services = [
+  {
+    title: "Depilación Láser",
+    description: "Elimina el vello de forma permanente con nuestra tecnología avanzada.",
+    image: "/images/servicios/depilacion-laser.jpg"
+  },
+  {
+    title: "Faciales",
+    description: "Luce una piel radiante con nuestros tratamientos faciales personalizados.",
+    image: "/images/servicios/faciales.jpg"
+  }
+];
+
+
+export default services;
