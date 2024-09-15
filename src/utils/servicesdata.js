f9182c6954662983a791e9b26ab9a4682d4abea6
alexandro.berdejo <alexandro.berdejo@gmail.com>
2024-09-15 04:28:52 -0600

    'heroynavbarhecho'

---
+++ src/utils/servicesdata.js
@@ -0,0 +1,89 @@
+export const treatments = [
+    {
+      title: "Botox | Baby Botox",
+      description: "Tratamiento inyectable que ayuda a prevenir y tratar arrugas de la expresión.",
+      duration: "35min",
+      effectDuration: "4 meses",
+      postCare: [
+        "No exponerse al sol",
+        "No manipularse por 4hrs posterior a la aplicación",
+        "Mantener una posición vertical por 4hrs posterior a la aplicación"
+      ]
+    },
+    {
+      title: "Bioestimuladores",
+      description: "Sustancias inyectables que estimulan la producción de colágeno en la piel.",
+      duration: "35min",
+      effectDuration: "Resultados hasta 12 meses",
+      postCare: [
+        "No exponerse al sol",
+        "No manipularse por 4hrs posterior a la aplicación"
+      ]
+    },
+    {
+      title: "Rellenos ácido hialurónico",
+      description: "Tratamiento inyectable para dar volumen, mejorar y corregir diferentes áreas faciales.",
+      duration: "40min",
+      effectDuration: "8-12 meses",
+      postCare: [
+        "No exponerse al sol",
+        "No manipular 24 hrs posteriores",
+        "No colocar frío o calor en la zona tratada",
+        "Vigilancia durante 48hrs"
+      ]
+    },
+    {
+      title: "Lipoenzimas",
+      description: "Tratamiento inyectable a base de enzimas que ayuda a reducir grasa localizada.",
+      duration: "25min",
+      effectDuration: "Permanente con hábitos alimenticios",
+      postCare: [
+        "No exponerse al sol",
+        "No manipularse posterior a la aplicación"
+      ]
+    },
+    {
+      title: "Limpieza facial hidra",
+      description: "Limpieza facial grado médico estético no invasivo que limpia, exfolia e hidrata la piel del rostro.",
+      duration: "45-50min",
+      effectDuration: "Se recomienda una sesión al mes o bimestral",
+      postCare: [
+        "No exponerse al sol",
+        "No manipularse posterior a la aplicación",
+        "Aplicarse protector solar"
+      ]
+    },
+    {
+      title: "Dermapen",
+      description: "Tratamiento estético que mejora la textura y el aspecto de la piel a través de micropunciones.",
+      duration: "45-50min",
+      effectDuration: "Se recomienda una sesión cada 7 o 15 días",
+      postCare: [
+        "No exponerse al sol",
+        "No manipularse posterior a la aplicación",
+        "Aplicarse protector solar"
+      ]
+    },
+    {
+      title: "Armonización facial",
+      description: "Conjunto de técnicas que buscan equilibrar las proporciones y características del rostro del paciente.",
+      duration: "15-45min",
+      effectDuration: "Personalizado según necesidades del paciente",
+      postCare: [
+        "No exponerse al sol",
+        "No manipularse posterior a la aplicación",
+        "Aplicarse protector solar",
+        "Dependientes de los tratamientos a realizar"
+      ]
+    },
+    {
+      title: "Depilación láser diodo",
+      description: "Depilación a largo plazo que puede llegar a ser definitiva usando láser de diodo.",
+      duration: "Variable",
+      effectDuration: "10 sesiones cada 30-45 días",
+      postCare: [
+        "No exponerse al sol",
+        "Aplicarse protector solar y aloe vera"
+      ]
+    }
+  ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./src/**/*.{astro,html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-coffee': '#efe0cc',
+        'custom-beige': '#fbf1e4',
+        'custom-dark-blue': '#7cbcfc',
+        'custom-light-blue': '#c3f9fc',
+        'custom-light-beige': '#F5F5DC',
+        'custom-light-gray': '#D3D3D3',
+      },
+      fontFamily: {
+        cursive: ['Brush Script MT', 'cursive'],
+      },
+      screens: {
+        'xs': '480px',
+        'sm': '640px',
+        'md': 'px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+        'hide-img': {'min': '200px', 'max': '1024px'},
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-clip-path'),
+  ],
 }
