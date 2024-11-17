01264081b2c4de0188823e77c58c352fa9c83c4e
AlexBerdejo7 <alexandro.berdejo@gmail.com>
2024-11-17 13:20:14 -0600

    redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://drajessicaalcala.com',
+  integrations: [tailwind()],
+  redirects: {
+    'https://drajessicaalcala.netlify.app/*': {
+      destination: 'https://drajessicaalcala.com/:splat',
+      permanent: true
+    },
+    'https://www.drajessicaalcala.com/*': {
+      destination: 'https://drajessicaalcala.com/:splat',
+      permanent: true
+    }
+  }
 });
