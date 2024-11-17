5a26b549922eb081cf147db79cb2113dff8a1e7a
AlexBerdejo7 <alexandro.berdejo@gmail.com>
2024-11-17 13:23:39 -0600

    redirects2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,7 @@ export default defineConfig({
   site: 'https://drajessicaalcala.com',
   integrations: [tailwind()],
   redirects: {
-    'https://drajessicaalcala.netlify.app/*': {
-      destination: 'https://drajessicaalcala.com/:splat',
-      permanent: true
-    },
-    'https://www.drajessicaalcala.com/*': {
-      destination: 'https://drajessicaalcala.com/:splat',
-      permanent: true
-    }
+    '/drajessicaalcala.netlify.app': 'https://drajessicaalcala.com',
+    '/www.drajessicaalcala.com': 'https://drajessicaalcala.com'
   }
 });
